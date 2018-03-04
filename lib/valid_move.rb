9a9d1286fb44b470ac722060426a33f75de2573f
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-04 14:46:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
-def valid_move?(board, index)
-  index = position.to_i - 1
+def valid_move?(board, position)
+  .to_i - 1
   if index.between?(0,8)
     true
   else
