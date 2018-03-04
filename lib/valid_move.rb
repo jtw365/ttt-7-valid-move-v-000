5a599efd7a15799cb9e1e4ed0ef06bb434ec1ded
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-04 15:13:36 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-  if position.between?(0,9) && !position_taken?(board, index)
+  if position.between?(0,9) && !position_taken?(board, position)
     true
   else
     false
